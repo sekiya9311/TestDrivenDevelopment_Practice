b0a70192af21b56c1fbd7aabb7b32ab55990e79b
sekiya9311 <sekiya9311@gmail.com>
2018-10-14 17:04:49 +0900

    Chaper1

---
+++ 01_MoneyExample/Dollar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_MoneyExample
+{
+    public class Dollar
+    {
+        public int amount;
+
+        public Dollar(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void Times(int multiplier)
+        {
+            amount *= multiplier;
+        }
+
+    }
+}
---
+++ 01_MoneyExampleTest/MoneyTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using _01_MoneyExample;
+
+namespace _01_MoneyExampleTest
+{
+    public class MoneyTest
+    {
+        [Fact]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.Equal(10, five.amount);
+        }
+    }
+}
